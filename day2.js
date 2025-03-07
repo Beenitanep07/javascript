f46c5cece8e1645ec8cefbf6f4638b728facf958
Beenita Neupane <beenita.neupane@wisherstech.com>
2025-03-07 19:07:26 +0545

    first commit

---
+++ day2.js
@@ -0,0 +1,40 @@
+//output data in console
+//  console.log('hello world');
+
+//run code in nodejs
+//node day2.js
+ 
+ //watch mode (automatically rerun or restart)
+ //  node --watch day2.js
+
+ //variable and datatype
+ //variable is the container that store value
+ //datatye type of the data i.e string,number,boolean
+ //string datatype
+ let cup = "tea" //string
+ let vessel = "flower" //string
+let bag = "book"
+
+//number i.e. integer data type
+ let test = 1
+
+ //constant
+ const pi = 3.14 //number
+ const gravity = 9.8 //number
+
+ const password = "thisissecrectpassword"
+
+
+ //bolean data type
+let isNepali = true //boolean data type
+let isIndian = false //boolean data type
+
+
+//concatenation
+console.log("what do i have in my vessel " + vessel)
+console.log(` what do i have in my vessel  ${vessel}` ) // this is backtick sign not an single inveted sign....this is done to fetch dynamic value
+
+
+let age = 22;
+console.log("My age is " + age)
+console.log(`My age is ${age}`)
